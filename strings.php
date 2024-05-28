b53c71722d1cc666a9473361b3b4fef975516f4d
hemantDwivedi <hemantdwiwedi@gmail.com>
2024-05-28 10:06:31 +0530

    learning php day 3

---
+++ strings.php
@@ -0,0 +1,21 @@
+<?php
+
+# returns length of a string
+echo strlen("Hello");
+echo "<br>";
+
+# counts the number of word in a string
+echo str_word_count("Hello World Hello php");
+echo "<br>";
+
+# reverse a string
+echo strrev("Hello world");
+echo "<br>";
+
+# string concatenation
+$x = "Hello";
+$y = "World";
+
+echo $x." ".$y;
+echo "<br>";
+?>
